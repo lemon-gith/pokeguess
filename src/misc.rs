f4d2be1b78c18439ada91f7ac4665392a2460e90
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2024-04-14 00:22:03 +0100

    initialise repo

---
+++ src/misc.rs
@@ -0,0 +1,20 @@
+// not to be included in main, just to run here, as is
+
+/*
+1. Convert temperatures between Fahrenheit and Celsius.
+2. Generate the nth Fibonacci number.
+ */
+
+fn temp_converter(){
+
+}
+
+fn fibonacci_generator(){
+
+}
+
+fn caller(){
+
+}
+
+// fn main(){ caller(temp_converter); }

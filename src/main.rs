dfdc97f6135389fa74491c853f34f6d0172fbaf8
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2024-07-02 19:11:36 +0100

    add an extra newline

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod caller;
 
 
 fn main() {
-    println!("{}\n", format!("{:-^100}", " Welcome to my first toolbox "));
+    println!("\n{}\n", format!("{:-^100}", " Welcome to my first toolbox "));
 
     caller::caller();
 }
